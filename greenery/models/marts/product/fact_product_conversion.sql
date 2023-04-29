edde44ff68da0fcb2006022b7b8ac4d4198c2836
Isabela Lemos <isabelalemos@live.com>
2023-04-29 18:00:51 +0000

    Added user conversion sql and yml files

--- greenery/models/marts/product/fact_product_conversion.sql
+++ greenery/models/marts/product/fact_product_conversion.sql
@@ -3,11 +3,13 @@ SELECT
     , de.product_guid
     , p.product_name
     , de.total_pageviews
+    , de.total_sessions_with_pageviews
     , de.total_add_to_cart
     , do.total_orders_with_product
     , ROUND(total_add_to_cart/total_pageviews, 2) AS conversion_pageview_to_cart
     , ROUND(total_orders_with_product/total_add_to_cart,2) AS conversion_cart_to_order
     , ROUND(total_orders_with_product/total_pageviews,2) AS conversion_pageview_to_order
+    , ROUND(total_orders_with_product/total_sessions_with_pageviews,2) AS conversion_session_to_order
 
 FROM {{ ref('int_daily_events_per_product') }} AS de
 LEFT JOIN {{ ref('stg_postgres__products') }} AS p
